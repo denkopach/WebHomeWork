691590aa543f48805b75e908ecfd413643d75e99
Den Kopach <denkopach@yahoo.com>
2018-03-28 22:26:26 +0300

    corrected according to comments

--- WD_PS2/script.js
+++ WD_PS2/script.js
@@ -1,85 +1,76 @@
-function funonload(){
-	getResultTask1();
-	getResultTask2();
-	getResultTask3()
-}
-
 function getResultTask1(){
 	var amount = 0;
 	for(var i = -1000; i <= 1000; i++){
 		amount = amount + i;
 	}
-	document.getElementById('task1-result').value = amount;
+	addResult(amount, 'task1');
+}
+
+function addResult(result, id){
+	var spanResult = document.getElementById('result-' + id);
+	spanResult.className = 'span-result';
+	spanResult.innerText = result;
 }
 
 function getResultTask2(){
 	var amount = 0;
-	var n = 0;
 	for(var i = -1000; i <= 1000; i++){
-		if(
-			(Math.abs(i) + 10) % 10 == 2 || 
-			(Math.abs(i) + 10) % 10 == 3 || 
-			(Math.abs(i) +10) % 10  == 7 ){
+		if((Math.abs(i)) % 10 === 2
+			|| (Math.abs(i)) % 10 === 3
+			|| (Math.abs(i)) % 10  === 7)
 			amount = amount + i;
-		}
 	}
-	document.getElementById('task2-result').value = amount;
+	addResult(amount, 'task2');
 }
 
 function getResultTask3(){
-	document.getElementById('task3').innerHTML = '<span></span>';
+	var span = document.createElement('span');
+	var task3 = document.getElementById('task3');
+	task3.innerText = '';
+	task3.appendChild(span);
 	var task3Div = document.getElementById('task3');
 	var stars = "";
 	for(var i = 0; i < 50; i++){
-			for(var j = 0; j <= i; j++){
-				stars = stars + '*';
-			}
-	var span = document.createElement('span');
-	var br = document.createElement('br');
-	span.className = "span-task3";
-	span.innerHTML = stars + '</br>';
-	insertAfter(span, task3Div.lastChild);
-	stars = "";
+		for(var j = 0; j <= i; j++){
+			stars = stars + '*';
+		}
+		var br = document.createElement('br');
+		span = document.createElement('span');
+		span.className = "span-task3";
+		span.innerText = stars;
+		span.appendChild(br);
+		task3Div.appendChild(span);
+		stars = "";
 	}
-}
-
-
-function insertAfter(elem, refElem) {
-	return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
 }
 
 function getResultTask4(){
 	var input = document.getElementById('task4-input').value;
-	if(isValidateInt(input)){
-		validationInputGood('task4');
+	if(isInt(input)){
+		clearErrMsg('task4');
 		var result = [];
 		result[0] = addingLeadingZeros(String(Math.floor(input / 3600)), 2);
 		result[1] = addingLeadingZeros(String(Math.floor((input % 3600) / 60)), 2);
 		result[2] = addingLeadingZeros(String(input % 60), 2);
-		document.getElementById('task4-result').value = result[0] + ':' + result[1] + ':' + result[2];
+		addResult(result[0] + ':' + result[1] + ':' + result[2], 'task4');
 	} else {
-		validationInputErr('enter an integer', 'task4');
+		addErrMsg('enter an integer', 'task4');
 	}
 }
 
-function isValidateInt(input){
-	var parsedInput = parseInt(input);
-	if(input == '' || parsedInput != input) return false;
-	return true;
-}
-
-function validationInputErr(message, id){
+function addErrMsg(message, id){
 	document.getElementById('block-error-' + id).innerHTML = message;
 	document.getElementById('block-error-' + id).className = 'block-error-invalid';
 	document.getElementById(id + '-input').className = 'input-is-error';
 	document.getElementById(id + '-input').value = '';
 }
 
-function validationInputGood(id){
+function clearErrMsg(id){
 	document.getElementById('block-error-' + id).className = 'block-error-valid';
 	document.getElementById(id + '-input').className = '';
 }
 
+//Universal function for appending leading zeros
 function addingLeadingZeros(val, len){
 	if(val.length < len){
 		val = '0' + val;
@@ -90,13 +81,14 @@ function addingLeadingZeros(val, len){
 
 function getResultTask5(){
 	var input = document.getElementById('task5-input').value;
-	if(isValidateInt(input)){
-		validationInputGood('task5');
+	if(isInt(input)){
+		clearErrMsg('task5');
 		var age = Number(input);
 		var res = getStringNameForIntVal(age, ' год', ' года', ' лет');
-		document.getElementById('task5-result').value = res;
+		addResult(res, 'task5');
+
 	} else {
-		validationInputErr('enter an integer', 'task4');
+		addErrMsg('enter an integer', 'task4');
 	}
 }
 
@@ -105,12 +97,12 @@ function getResultTask6(){
 	var inputSecond = document.getElementById('task6-second-input').value;
 	var errMsg = "enter date in format: October 13, 2014 11:13:00";
 	if (!isValidDateTask6(inputFirst)){
-		validationInputErr(errMsg, 'task6-first');
+		addErrMsg(errMsg, 'task6-first');
 	} else if (!isValidDateTask6(inputSecond)){
-		validationInputErr(errMsg, 'task6-second');
+		addErrMsg(errMsg, 'task6-second');
 	} else {
-		validationInputGood('task6-first');
-		validationInputGood('task6-second');
+		clearErrMsg('task6-first');
+		clearErrMsg('task6-second');
 		var timeFirst = convertToDate(inputFirst);	
 		var timeSecond = convertToDate(inputSecond);
 		if(timeFirst != 'Invalid Date' && timeSecond != 'Invalid Date'){
@@ -123,13 +115,15 @@ function getResultTask6(){
 				Math.floor(((((timeDifference % 31536000000) % 2592000000) % 86400000) % 3600000) / 60000),
 				Math.floor((((((timeDifference % 31536000000) % 2592000000) % 86400000) % 3600000) % 60000) / 1000)
 			];
-			document.getElementById('task6-result').value = 
+			var res = 
 				getStringNameForIntVal(result[0], ' год, ', ' года, ', ' лет, ') +
 				getStringNameForIntVal(result[1], ' месяц, ', ' месяца, ', ' месяцев, ') +
 				getStringNameForIntVal(result[2], ' день, ', ' дня, ', ' дней, ') +
 				getStringNameForIntVal(result[3], ' час, ', ' часа, ', ' часов, ' ) +
 				getStringNameForIntVal(result[4], ' минута, ', ' минуты, ', ' минут, ' ) +
 				getStringNameForIntVal(result[5], ' секунда, ', ' секунды, ', ' секунд' );
+			addResult(res, 'task6');
+
 		}
 	}
 }
@@ -138,7 +132,7 @@ function getStringNameForIntVal(num, str1, str2, str3){
 	var res = '';
 	if(num > 4 && num < 21)
 		return num + str3;
-	else if((num + 10) % 10 == 1)
+	else if((num + 10) % 10 === 1)
 		return num + str1;
 	else if((num + 10) % 10 > 1 && (num + 10) % 10 < 5)
 		return num + str2;
@@ -164,12 +158,12 @@ function isValidDateTask6(input){
 	];
 	var date = convertToDate(input);
 	input = input.split(/,| |:/);
-	return  date.getFullYear() == input[3] &&
-			date.getDate()     == input[1] &&
-			date.getHours()	   == input[4] &&
-			date.getMinutes()  == input[5] &&
-			date.getSeconds()  == input[6] &&
-			arr[date.getMonth()] == input[0];
+	return  date.getFullYear()      === input[3]
+			&& date.getDate()       === input[1]
+			&& date.getHours()	    === input[4] 
+			&& date.getMinutes()    === input[5]
+			&& date.getSeconds()    === input[6]
+			&& arr[date.getMonth()] === input[0];
 }
 
 function convertToDate(inputText){
@@ -180,29 +174,31 @@ function convertToDate(inputText){
 	
 function getResultTask7(){
 	var input = document.getElementById('task7-input').value;
+	document.getElementById('task7-input').value = '';
 	var date = 	convertToDate(input);
-	if(date == 'Invalid Date' || !isValidDateTask7(input)) {
-		validationInputErr('enter date in format\: 2014-12-31', 'task7');
+	if(date === 'Invalid Date' || !isValidDateTask7(input)) {
+		addErrMsg('enter date in format\: 2014-12-31', 'task7');
+		return false;
 	}
-	validationInputGood('task7');
+	clearErrMsg('task7');
 	var day = date.getDate();
 	var month = date.getMonth()+1;
-	if (day < 1 || day > 31) validationInputErr('enter date in format: 2014-12-31', 'task7');
-	else if (month==2 && day>29) validationInputErr('enter date in format: 2014-12-31', 'task7');
-	else if ((month==4||month==6||month==9||month==11) && day>30) validationInputErr('enter date in format: 2014-12-31', 'task4');
-	else if (month==1 && day>=20 || month==2 && day<=18) result="Водолей";
-	else if (month==2 && day>=19 || month==3 && day<=20) result="Рыбы";
-	else if (month==3 && day>=21 || month==4 && day<=19) result="Овен";
-	else if (month==4 && day>=20 || month==5 && day<=20) result="Телец";
-	else if (month==5 && day>=21 || month==6 && day<=21) result="Близнецы";
-	else if (month==6 && day>=22 || month==7 && day<=22) result="Рак";
-	else if (month==7 && day>=23 || month==8 && day<=22) result="Лев";
-	else if (month==8 && day>=23 || month==9 && day<=22) result="Дева";
-	else if (month==9 && day>=23 || month==10 && day<=22) result="Весы";
-	else if (month==10 && day>=23 || month==11 && day<=21) result="Скорпион";
-	else if (month==11 && day>=22 || month==12 && day<=21) result="Стрелец";
-	else if (month==12 && day>=22 || month==1 && day<=19) result="Козерог";
-	else validationInputErr('enter date in format: 2014-12-31', 'task7');
+	if (day < 1 || day > 31) addErrMsg('enter date in format: 2014-12-31', 'task7');
+	else if (month===2 && day>29) addErrMsg('enter date in format: 2014-12-31', 'task7');
+	else if ((month==4||month==6||month==9||month==11) && day>30) addErrMsg('enter date in format: 2014-12-31', 'task4');
+	else if (month===1 && day>=20  || month===2 && day<=18) result="Водолей";
+	else if (month===2 && day>=19  || month===3 && day<=20) result="Рыбы";
+	else if (month===3 && day>=21  || month===4 && day<=19) result="Овен";
+	else if (month===4 && day>=20  || month===5 && day<=20) result="Телец";
+	else if (month===5 && day>=21  || month===6 && day<=21) result="Близнецы";
+	else if (month===6 && day>=22  || month===7 && day<=22) result="Рак";
+	else if (month===7 && day>=23  || month===8 && day<=22) result="Лев";
+	else if (month===8 && day>=23  || month===9 && day<=22) result="Дева";
+	else if (month===9 && day>=23  || month===10 && day<=22) result="Весы";
+	else if (month===10 && day>=23 || month===11 && day<=21) result="Скорпион";
+	else if (month===11 && day>=22 || month===12 && day<=21) result="Стрелец";
+	else if (month===12 && day>=22 || month===1 && day<=19) result="Козерог";
+	else addErrMsg('enter date in format: 2014-12-31', 'task7');
 
 	var imgName;
 	switch(result) {
@@ -243,46 +239,48 @@ function getResultTask7(){
 			imgName = '12';
 			break;
 	}
-	document.getElementById('task7-result').value = result;
+	addResult(result, 'task7');
 	document.getElementById('task7-iconsZodiak').src = "icons/" + imgName + '.png';
 }
 
 function isValidDateTask7(str){
+	var re = /^(19|20)[0-9]{2}-[0|1][0-9]-[0-3][0-9]/;
+	if(!re.test(str)) {
+		return false;
+	}
 	if (!str) {
 		return false;
 	}
-    str = str.split("-");
-    if (!str[1] || !str[2]){
-    	return false;
-    }
-    for (var i = str.length; i--;) {
-    	if (isNaN(str[i]) || !str[i]){
-    		return false;
-    	}
-    }
-    return true;
+	str = str.split("-");
+	if (!str[1] || !str[2]){
+		return false;
+	}
+	return true;
 }
 
 function getResultTask8(){
 	var input = document.getElementById('task8-input').value;
+	document.getElementById('task8-input').value = '';
 	input = input.split(/x/);
+
 	if(isValidInputTask8(input)){
-		validationInputGood('task8');
+		clearErrMsg('task8');
 		var task8Div = document.getElementById('div-task8-result');
+		task8Div.innerHTML = '';
 		for(var i = 0; i < input[0]; i++){
 			var task8DivNew = document.createElement('div');
 			var br = document.createElement('br');
 			task8DivNew.className = 'task8-line';
-			insertAfter(task8DivNew, task8Div.lastChild);
+			task8Div.appendChild(task8DivNew);
 			for(var j = 0; j < input[1]; j++){
 				var divCell = document.createElement('div');
-				if((j + i + 2) % 2 == 0) divCell.className = "div-task8-cell-odd";
+				if((j + i + 2) % 2 === 0) divCell.className = "div-task8-cell-odd";
 				else divCell.className = "div-task8-cell-even";
 				task8DivNew.appendChild(divCell);
 			}
 		}
 	}else {
-		validationInputErr('input format is: 8x8', 'task8');
+		addErrMsg('input format is: 8x8', 'task8');
 	}
 }
 
@@ -298,50 +296,49 @@ function isValidInputTask8(arr){
 
 function getResultTask9(){
 	var inputApartment = document.getElementById('task9-apartment-input').value;
-	if(!isValidateInt(inputApartment)){
-		validationInputErr('enter an integer', 'task9-apartment');
+	if(!isInt(inputApartment)){
+		addErrMsg('enter an integer', 'task9-apartment');
 		return false;
 	}
-	validationInputGood('task9-apartment');
+	clearErrMsg('task9-apartment');
 	var inputApartmentAmount = document.getElementById('task9-apartmentAmount-input').value;
-	if(!isValidateInt(inputApartmentAmount)){
-		validationInputErr('enter an integer', 'task9-apartmentAmount');
+	if(!isInt(inputApartmentAmount)){
+		addErrMsg('enter an integer', 'task9-apartmentAmount');
 		return false;
 	}
-	validationInputGood('task9-apartmentAmount');
+	clearErrMsg('task9-apartmentAmount');
 	var inputFloor = document.getElementById('task9-floors-input').value;
-	if(!isValidateInt(inputFloor)){
-		validationInputErr('enter an integer', 'task9-floors');
+	if(!isInt(inputFloor)){
+		addErrMsg('enter an integer', 'task9-floors');
 		return false;
 	}
-	validationInputGood('task9-floors');
+	clearErrMsg('task9-floors');
 	var numApartmentInEntrace = inputFloor * inputApartmentAmount;
 	var resultEntrance = Math.ceil(inputApartment / numApartmentInEntrace);
-	var resultFloor = Math.ceil((resultEntrance % numApartmentInEntrace) / inputApartmentAmount);
+	var resultFloor = Math.ceil( (inputApartment % numApartmentInEntrace) / inputApartmentAmount);
 	if(resultFloor == 0) resultFloor = inputFloor;
-	document.getElementById('task9-result').value = resultEntrance + ' entrance, ' + resultFloor + ' floor';
+	addResult(resultEntrance + ' entrance, ' + resultFloor + ' floor', 'task9');
 }
 
 function getResultTask10(){
 	var input = document.getElementById('task10-input').value;
-	if(isValidateInt(input)){
-		validationInputGood('task10-input');
-		var result = 0;
-		for(var i = 0; i < input.length; i++){
-			result += Number(input[i]);
-		}
-		document.getElementById('task10-result').value = result;
+	var arr = input.split('');
+	if(isInt(input)){
+		clearErrMsg('task10');
+		var result = input.split('').map(Number).reduce(function (a, b) {
+            return a + b;
+        }, 0);
+		addResult(result, 'task10');
 	} else {
-		validationInputErr('enter an integer', 'task10');
+		addErrMsg('enter an integer greater than zero', 'task10');
 	}
 }
 
 function getResultTask11(){
 	var input = document.getElementById('task11-textarea').value;
-	input = input.split(/,/);
-	for(var i = 0; i < input.length; i++){
-		input[i] = input[i].replace('http://', '').replace('https://', '');
-	}
+	input = input.split(',').map(function(element){
+		return element.replace('http://', '').replace('https://', '');
+	});
 	input = input.sort();
 	var ulTask = document.getElementById('task11-ul');
 	for(var i = 0; i < input.length; i++){
@@ -351,10 +348,6 @@ function getResultTask11(){
 	}
 }
 
-//format date is 2014-12-31
-function isValidDate(input){
-	var date = new Date(input[0], input[1] - 1, input[2]);
-	return date.getFullYear() == input[0] &&
-           date.getDate()     == input[2] &&
-           date.getMonth()    == input[1] - 1;
+function isInt(input){
+	return input && !isNaN(input) && Number.isInteger(Number(input)) && input > 0;
 }
