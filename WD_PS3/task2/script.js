79c943f9c1ad9445ef087a048799d85cc4dc1f65
Den Kopach <denkopach@yahoo.com>
2018-04-16 09:52:17 +0300

    made a change according to the commentary

--- WD_PS3/task2/script.js
+++ WD_PS3/task2/script.js
@@ -1,22 +1,39 @@
 $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+    
+    const page = $("html, body");
+
+    $('#menu').on('click','a', function (event) {
+        
         event.preventDefault();
         var id  = $(this).attr('href'),
-            top = $(id).offset().top - $("div.block-header").height();
-        $('body,html').animate({scrollTop: top}, 500);
-    });
-    
+            top = $(id).offset().top - $('div.block-header').height();
+        $(page).animate({scrollTop: top}, 500);
 
-    $('#up').on("click", function() {
-        $('body, html').animate({scrollTop: 0},500);
         return false;
     });
 
     $(window).scroll(function () {
+        
         if ($(this).scrollTop() > 100) {
             $('#up').show('slow');
         }else{
             $('#up').hide('slow');
         }
+
+        return false;
     });
+
+    $( "#up" ).click(function(e) {
+
+       page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+           page.stop();
+       });
+
+       page.animate({ scrollTop: 0 }, 'slow', function(){
+           page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+       });
+
+       return false; 
+    });
+
 })
