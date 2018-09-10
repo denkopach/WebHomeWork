2b3c15520802fc24dad4678d4915eb63cbb3eda1
Den Kopach <denkopach@yahoo.com>
2018-09-10 12:14:46 +0300

    implemented backend

--- WD_ST3/js/script.js
+++ WD_ST3/js/script.js
@@ -4,6 +4,7 @@ const ESC_KEY = 27
 const TOP_INDENTATION = 40
 const LEFT_INDENTATION = 8
 
+let id = 0;
 
 function checkInputTextLen(text) {
 	return text.length >= MIN_LEN;
@@ -22,24 +23,74 @@ function determineShift(bubble) {
 	$(bubble).offset({top: top, left: left}) 
 }
 
-function saveMsg(bubble, inputText, key = ENTER_KEY){
+function editMsg(bubble, inputText, key = ENTER_KEY){
 	if (key === ENTER_KEY && checkInputTextLen(inputText)) {
 		$(bubble).text(inputText)
 			.addClass('bubble-ready')
 			.find('input').remove()
 		determineShift(bubble)
+		updMsg(bubble)
 	} else if (key === ENTER_KEY && !checkInputTextLen(inputText)){
-		bubble.remove();
+		bubble.isDel = 1
+		updMsg(bubble)
+		$(bubble).empty().remove();
 	} else if (key === ESC_KEY) {
 		$(bubble).text(inputText)
-		console.log(inputText)
 		$(bubble).find('input').remove();
 	}
 	return;
 }
 
+function updMsg(obj) {
+	if (!obj.isDel || obj.isDel === null) {
+		obj.isDel = 0;
+	}
+	$.ajax({
+		url: "php/app.php",
+		method: "POST",
+		data: {
+			msg: $(obj).text(),
+			id: $(obj).attr('id'),
+			offsetX: $(obj).offset().left + 8,
+			offsetY: $(obj).offset().top + 40,
+			isDel: obj.isDel,
+		},
+	})
+}
+
+function getAllMsg() {
+	$.ajax({
+		url: "php/app.php",
+		method: "POST",
+		dataType: "json",
+		data: {
+			getAllMsg: true,
+		},
+	}).done(function (res) {
+		for (let i in res) {
+			addBubble(res[i])
+			id = Number(res[i].id) + 1;
+		}
+	})
+}
+function createBubble(obj) {
+	return $('<div/>')
+		.addClass('bubble')
+		.attr('id', id)
+		.css({top: obj.offsetY - TOP_INDENTATION, left: obj.offsetX - LEFT_INDENTATION})
+		.appendTo('.main')
+		.draggable({
+			containment: 'parent' 
+		})
+}
+function addBubble(obj) {
+	const bubble = createBubble(obj)
+	$(bubble).text(obj.msg)
+		.addClass('bubble-ready')
+}
+
 $(function(){
-	let id = 0;
+	getAllMsg();
 	$('.main').dblclick(function(e){	
 		const bubble = e.target;
 
@@ -51,14 +102,16 @@ $(function(){
 				.appendTo(e.target)
 				.focus()
 				.blur(function() {
-					saveMsg(bubble, $(this).val(), ENTER_KEY);
+					editMsg(bubble, $(this).val(), ENTER_KEY)
 				})
 				.keydown(function(e) {
-					saveMsg(bubble, $(this).val(), e.keyCode);
+					editMsg(bubble, $(this).val(), e.keyCode)
 				})
 		} else {
 			const inputEl = $('<input/>');
 			const bubble = createBubble(e)
+			bubble.isDel = 0
+			id++
 			inputEl.addClass('signature')
 				.appendTo(bubble)
 				.focus()
@@ -67,23 +120,23 @@ $(function(){
 						bubble.remove();
 						return
 					}
-					saveMsg(bubble, $(this).val(), e.keyCode);
+					editMsg(bubble, $(this).val(), e.keyCode);
 				})
 				.blur(function() {
-					saveMsg(bubble, $(this).val(), ENTER_KEY);
+					editMsg(bubble, $(this).val(), ENTER_KEY);
 				})
+
 		}
 	})
-
-function createBubble(e) {
-	return $('<div/>')
-		.addClass('bubble')
-		.attr('id', id++)
-		.css({top: e.offsetY - TOP_INDENTATION, left: e.offsetX - LEFT_INDENTATION})
-		.appendTo('.main')
-		.draggable({
-			containment: 'parent' 
-		})
-}
-
+	// $('.bubble').mouseup(function() {
+	// 	editMsg($(this), $(this).textContent(), e.keyCode)
+	// })
+	$(".main").droppable({
+		// drop: function(event, ui) {
+		// 	console.log($(ui.draggable).text())
+		// }
+		drop: function(event, ui) {
+			editMsg(ui.draggable, $(ui.draggable).text())
+		}
+	});
 })
