5d22b55331f1ac1f2226ce889b33d2a9824f57c4
Den Kopach <denkopach@yahoo.com>
2018-09-10 14:17:03 +0300

    implemented backend

--- WD_ST3/js/script.js
+++ WD_ST3/js/script.js
@@ -7,131 +7,131 @@ const LEFT_INDENTATION = 8
 let id = 0;
 
 function checkInputTextLen(text) {
-	return text.length >= MIN_LEN;
+    return text.length >= MIN_LEN;
 }
 
 function determineShift(bubble) {
-	let left = $(bubble).offset().left
-	let top = $(bubble).offset().top
-	const cont = $('.main')
-	if (left + $(bubble).outerWidth(true) > cont.outerWidth(true)) {
-		left = cont.outerWidth(true) - $(bubble).outerWidth(true)
-	}
-	if (top + TOP_INDENTATION - $(bubble).outerHeight() < 0) {
-		top = $(bubble).outerHeight() - TOP_INDENTATION
-	}
-	$(bubble).offset({top: top, left: left}) 
+    let left = $(bubble).offset().left
+    let top = $(bubble).offset().top
+    const cont = $('.main')
+    if (left + $(bubble).outerWidth(true) > cont.outerWidth(true)) {
+        left = cont.outerWidth(true) - $(bubble).outerWidth(true)
+    }
+    if (top + TOP_INDENTATION - $(bubble).outerHeight() < 0) {
+        top = $(bubble).outerHeight() - TOP_INDENTATION
+    }
+    $(bubble).offset({top: top, left: left}) 
 }
 
 function editMsg(bubble, inputText, key = ENTER_KEY){
-	if (key === ENTER_KEY && checkInputTextLen(inputText)) {
-		$(bubble).text(inputText)
-			.addClass('bubble-ready')
-			.find('input').remove()
-		determineShift(bubble)
-		updMsg(bubble)
-	} else if (key === ENTER_KEY && !checkInputTextLen(inputText)){
-		bubble.isDel = 1
-		updMsg(bubble)
-		$(bubble).empty().remove();
-	} else if (key === ESC_KEY) {
-		$(bubble).text(inputText)
-		$(bubble).find('input').remove();
-	}
-	return;
+    if (key === ENTER_KEY && checkInputTextLen(inputText)) {
+        $(bubble).text(inputText)
+            .addClass('bubble-ready')
+            .find('input').remove()
+        determineShift(bubble)
+        updMsg(bubble)
+    } else if (key === ENTER_KEY && !checkInputTextLen(inputText)){
+        bubble.isDel = 1
+        updMsg(bubble)
+        $(bubble).empty().remove();
+    } else if (key === ESC_KEY) {
+        $(bubble).text(inputText)
+        $(bubble).find('input').remove();
+    }
+    return;
 }
 
 function updMsg(obj) {
-	if (!obj.isDel || obj.isDel === null) {
-		obj.isDel = 0;
-	}
-	$.ajax({
-		url: "php/app.php",
-		method: "POST",
-		data: {
-			msg: $(obj).text(),
-			id: $(obj).attr('id'),
-			offsetX: $(obj).offset().left + 8,
-			offsetY: $(obj).offset().top + 40,
-			isDel: obj.isDel,
-		},
-	})
+    if (!obj.isDel || obj.isDel === null) {
+        obj.isDel = 0;
+    }
+    $.ajax({
+        url: "php/app.php",
+        method: "POST",
+        data: {
+            msg: $(obj).text(),
+            id: $(obj).attr('id'),
+            offsetX: $(obj).offset().left + 8,
+            offsetY: $(obj).offset().top + 40,
+            isDel: obj.isDel,
+        },
+    })
 }
 
 function getAllMsg() {
-	$.ajax({
-		url: "php/app.php",
-		method: "POST",
-		dataType: "json",
-		data: {
-			getAllMsg: true,
-		},
-	}).done(function (res) {
-		for (let i in res) {
-			addBubble(res[i])
-			id = Number(res[i].id) + 1;
-		}
-	})
+    $.ajax({
+        url: "php/app.php",
+        method: "POST",
+        dataType: "json",
+        data: {
+            getAllMsg: true,
+        },
+    }).done(function (res) {
+        for (let i in res) {
+            addBubble(res[i])
+            id = Number(res[i].id) + 1;
+        }
+    })
 }
 function createBubble(obj) {
-	return $('<div/>')
-		.addClass('bubble')
-		.attr('id', id)
-		.css({top: obj.offsetY - TOP_INDENTATION, left: obj.offsetX - LEFT_INDENTATION})
-		.appendTo('.main')
-		.draggable({
-			containment: 'parent' 
-		})
+    return $('<div/>')
+        .addClass('bubble')
+        .attr('id', id)
+        .css({top: obj.offsetY - TOP_INDENTATION, left: obj.offsetX - LEFT_INDENTATION})
+        .appendTo('.main')
+        .draggable({
+            containment: 'parent' 
+        })
 }
 function addBubble(obj) {
-	const bubble = createBubble(obj)
-	$(bubble).text(obj.msg)
-		.addClass('bubble-ready')
+    const bubble = createBubble(obj)
+    $(bubble).text(obj.msg)
+        .addClass('bubble-ready')
 }
 
 $(function(){
-	getAllMsg();
-	$('.main').dblclick(function(e){	
-		const bubble = e.target;
+    getAllMsg();
+    $('.main').dblclick(function(e){    
+        const bubble = e.target;
 
-		if (e.target.className.indexOf('bubble') + 1) {
-			let text = e.target.textContent;
-			e.target.textContent = '';
-			$('<input/>').addClass('signature')	
-				.val(text)
-				.appendTo(e.target)
-				.focus()
-				.blur(function() {
-					editMsg(bubble, $(this).val(), ENTER_KEY)
-				})
-				.keydown(function(e) {
-					editMsg(bubble, $(this).val(), e.keyCode)
-				})
-		} else {
-			const inputEl = $('<input/>');
-			const bubble = createBubble(e)
-			bubble.isDel = 0
-			id++
-			inputEl.addClass('signature')
-				.appendTo(bubble)
-				.focus()
-				.keydown(function(e) {
-					if (e.keyCode === ESC_KEY) {
-						bubble.remove();
-						return
-					}
-					editMsg(bubble, $(this).val(), e.keyCode);
-				})
-				.blur(function() {
-					editMsg(bubble, $(this).val(), ENTER_KEY);
-				})
+        if (e.target.className.indexOf('bubble') + 1) {
+            let text = e.target.textContent;
+            e.target.textContent = '';
+            $('<input/>').addClass('signature') 
+                .val(text)
+                .appendTo(e.target)
+                .focus()
+                .blur(function() {
+                    editMsg(bubble, $(this).val(), ENTER_KEY)
+                })
+                .keydown(function(e) {
+                    editMsg(bubble, $(this).val(), e.keyCode)
+                })
+        } else {
+            const inputEl = $('<input/>');
+            const bubble = createBubble(e)
+            bubble.isDel = 0
+            id++
+            inputEl.addClass('signature')
+                .appendTo(bubble)
+                .focus()
+                .keydown(function(e) {
+                    if (e.keyCode === ESC_KEY) {
+                        bubble.remove();
+                        return
+                    }
+                    editMsg(bubble, $(this).val(), e.keyCode);
+                })
+                .blur(function() {
+                    editMsg(bubble, $(this).val(), ENTER_KEY);
+                })
 
-		}
-	})
+        }
+    })
 
-	$(".main").droppable({
-		drop: function(event, ui) {
-			editMsg(ui.draggable, $(ui.draggable).text())
-		}
-	});
+    $(".main").droppable({
+        drop: function(event, ui) {
+            editMsg(ui.draggable, $(ui.draggable).text())
+        }
+    });
 })
--- WD_ST3/php/app.php
+++ WD_ST3/php/app.php
@@ -5,51 +5,51 @@ error_reporting(E_ALL);
 $dataPath = 'data.json';
 
 if (isset($_POST['getAllMsg'])) {
-	if (file_exists($dataPath) && !is_readable($dataPath)) {
-	    http_response_code(400);
-	    echo 'ERROR! File is not readable';
-	    return;
-	} if (file_exists($dataPath)) {
-		$allMsg = json_decode(file_get_contents($dataPath), true);
-	} else {
-		$allMsg = [];
-	}
-	$allMsg = array_filter($allMsg, function($msg){
-			return $msg['isDel'] != 1;
-		});
-	echo json_encode(
-			$allMsg, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-	die();
+    if (file_exists($dataPath) && !is_readable($dataPath)) {
+        http_response_code(400);
+        echo 'ERROR! File is not readable';
+        return;
+    } if (file_exists($dataPath)) {
+        $allMsg = json_decode(file_get_contents($dataPath), true);
+    } else {
+        $allMsg = [];
+    }
+    $allMsg = array_filter($allMsg, function($msg){
+            return $msg['isDel'] != 1;
+        });
+    echo json_encode(
+            $allMsg, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+    die();
 }
 
 if (isset($_POST['msg'])) {
-	if (file_exists($dataPath)){
-		$allMsgs = json_decode(file_get_contents($dataPath), true);
-	} else {
-		$allMsgs = [];
-	}
-	$msgExist = false;
-	$msgTmp = [
-		'msg' => $_POST['msg'],
-		'id' => $_POST['id'],
-		'offsetX' => $_POST['offsetX'],
-		'offsetY' => $_POST['offsetY'],
-		'isDel' => $_POST['isDel']
-	];
-	foreach ($allMsgs as $index => $msg) {
-		if ($msg['id'] === $_POST['id']) {
-			$allMsgs[$index] = $msgTmp;
-			$msgExist = true;
-		}
-	}
-	if (!$msgExist) {
-		$allMsgs[] = $msgTmp;
-	}
-	if (!file_put_contents($dataPath,json_encode(
-			$allMsgs, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT))) {
-		http_response_code(400);
-		echo 'ERROR! File with data can not be created or edit';
-	}
-	die();
+    if (file_exists($dataPath)){
+        $allMsgs = json_decode(file_get_contents($dataPath), true);
+    } else {
+        $allMsgs = [];
+    }
+    $msgExist = false;
+    $msgTmp = [
+        'msg' => $_POST['msg'],
+        'id' => $_POST['id'],
+        'offsetX' => $_POST['offsetX'],
+        'offsetY' => $_POST['offsetY'],
+        'isDel' => $_POST['isDel']
+    ];
+    foreach ($allMsgs as $index => $msg) {
+        if ($msg['id'] === $_POST['id']) {
+            $allMsgs[$index] = $msgTmp;
+            $msgExist = true;
+        }
+    }
+    if (!$msgExist) {
+        $allMsgs[] = $msgTmp;
+    }
+    if (!file_put_contents($dataPath,json_encode(
+            $allMsgs, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT))) {
+        http_response_code(400);
+        echo 'ERROR! File with data can not be created or edit';
+    }
+    die();
 }
 header('Location: ..' . DIRECTORY_SEPARATOR .'index.html');
